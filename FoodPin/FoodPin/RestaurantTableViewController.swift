27c6dbd3825e4e24be40ef4f444fa614d3b790e3
Michael Karr <mkarr1@yahoo.com>
2017-02-19 17:40:17 -0800

    Added images, implemented UITableViewController

---
+++ FoodPin/FoodPin/RestaurantTableViewController.swift
@@ -0,0 +1,51 @@
+//
+//  RestaurantTableViewController.swift
+//  FoodPin
+//
+//  Created by Michael Karr on 2/19/17.
+//  Copyright © 2017 Michael Karr. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantTableViewController: UITableViewController {
+    
+    var	restaurantNames	=	["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats And Deli", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and K"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return restaurantNames.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "Cell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        
+        //set cell image and configure cell
+        cell.textLabel?.text = restaurantNames[indexPath.row]
+        cell.imageView?.image = UIImage(named: "restaurant.jpg")
+        
+        return cell
+    }
+
+}
